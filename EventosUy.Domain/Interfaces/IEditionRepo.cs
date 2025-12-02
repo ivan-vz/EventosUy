b75dc651768711ceb4846bac1ae8f7e85838d9b0
Navi <ivanveci@hotmail.com>
2025-12-01 21:13:43 -0300

    Primera implementacion de Interfaz/Repo Edition

--- EventosUy.Domain/Interfaces/IEditionRepo.cs
+++ EventosUy.Domain/Interfaces/IEditionRepo.cs
@@ -4,10 +4,12 @@ namespace EventosUy.Domain.Interfaces
 {
     public interface IEditionRepo
     {
-        public Task<Edition> GetByIdAsync(Guid id);
-        public IEnumerable<Guid> GetAllAsync();
-        public IEnumerable<Guid> GetAllByInstitutionAsync(Guid institutionId);
-        public IEnumerable<Guid> GetAllByEventAsync(Guid eventId);
+        public Task<Edition?> GetByIdAsync(Guid id);
+        public Task<List<Guid>> GetAllAsync();
+        public Task<List<Guid>> GetAllByInstitutionAsync(Guid institutionId);
+        public Task<List<Guid>> GetAllByEventAsync(Guid eventId);
+        public Task<List<Guid>> GetAllPendingByEvent(Guid eventId);
+        public Task<bool> AlreadyExists(string name);
         public Task AddAsync(Edition edition);
         public Task RemoveAsync(Guid id);
     }
---
+++ EventosUy.Infrastructure/Repositorios/EditionRepo.cs
@@ -0,0 +1,40 @@
+﻿using EventosUy.Domain.Entities;
+using EventosUy.Domain.Enumerates;
+using EventosUy.Domain.Interfaces;
+
+namespace EventosUy.Infrastructure.Repositorios
+{
+    internal class EditionRepo : IEditionRepo
+    {
+        private HashSet<Edition> _editions;
+
+        public EditionRepo() { _editions = []; }
+
+        public Task AddAsync(Edition edition)
+        {
+            _editions.Add(edition);
+            return Task.CompletedTask;
+        }
+        
+        public Task<bool> AlreadyExists(string name) { return Task.FromResult(_editions.Any(edition => edition.Name.Equals(name, StringComparison.OrdinalIgnoreCase))); }
+
+        public Task<List<Guid>> GetAllAsync() { return Task.FromResult( _editions.Select(edition => edition.Id).ToList() ); }
+
+        public Task<List<Guid>> GetAllByEventAsync(Guid eventId) { return Task.FromResult(_editions.Where(edition => edition.Event == eventId).Select(edition => edition.Id).ToList()); }
+
+        public Task<List<Guid>> GetAllByInstitutionAsync(Guid institutionId) { return Task.FromResult(_editions.Where(edition => edition.Institution == institutionId).Select(edition => edition.Id).ToList()); }
+
+        public Task<List<Guid>> GetAllPendingByEvent(Guid eventId) 
+        {
+            return Task.FromResult(_editions.Where(edition => edition.Event == eventId && edition.State == EditionState.PENDING).Select(edition => edition.Id).ToList());
+        }
+
+        public Task<Edition?> GetByIdAsync(Guid id) { return Task.FromResult(_editions.SingleOrDefault(edition => edition.Id == id)); }
+
+        public Task RemoveAsync(Guid id)
+        {
+            _editions.RemoveWhere(edition => edition.Id == id);
+            return Task.CompletedTask;
+        }
+    }
+}
